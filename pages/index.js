ce1c7c77218af6ba0da79f05107dd8ed415f17cb
Lewis Freiberg <lewi@2pk.in>
2017-08-05 18:00:41 +1000

    Start padding my object out

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,20 @@
-export default props => <div>Herro</div>;
+import React from "react";
+import styled from "styled-components";
+
+import { seedGen } from "../libs/flash/iota";
+
+export default class extends React.Component {
+  state = {
+    one: { seed: seedGen(81) },
+    two: { seed: seedGen(81) },
+    flash: {
+      depth: 3,
+      counter: [0, 0, 0, 0],
+      bundles: []
+    }
+  };
+  render() {
+    console.log(this.state);
+    return <div>Herro</div>;
+  }
+}
