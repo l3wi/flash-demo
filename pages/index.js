f1992e8329ef196b6d526e22dbd40c90dc206260
Lewis Freiberg <lewi@2pk.in>
2017-08-06 00:30:05 +1000

    Runs some testing funcs on the index page

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 
-import { seedGen } from "../libs/flash/iota";
-import { webRTC } from "../libs/flash"
+import { seedGen, initiateAddress, finishAddress } from "../libs/flash/iota";
+import { calculateDepth, initialFlash, walkTree } from "../libs/flash/flash";
+
+import { webRTC } from "../libs/flash";
 
 export default class extends React.Component {
   state = {
     one: { seed: seedGen(81), input: 50 },
     two: { seed: seedGen(81), input: 50 },
     flash: {
-      depth: 3,
-      counter: [0, 0, 0],
+      depth: 5,
+      counter: [1, 2, 2, 2, 2],
       addressIndex: 0,
       addresses: [
         ["KJFJGFJHGJHG"],
         ["JKGFJKGFJGFHJGFJFK", "JGFGFUKFUFUKTFJFJHGIUHI"],
         ["TUKDYTDUTKDKTDKUTDUKYFYFYFFY", "QJGFQUFKAUSGVAVA"]
       ],
-      bundles: [{ lvl: 1 }, { lvl: 2 }, { lvl: 3 }]
+      bundles: []
     }
   };
+
+  componentDidMount() {
+    /// User one
+    var digest = initiateAddress({
+      seed: this.state.one.seed,
+      index: this.state.flash.addressIndex
+    });
+    // User two
+    var address = finishAddress(
+      {
+        seed: this.state.two.seed,
+        index: this.state.flash.addressIndex
+      },
+      digest
+    );
+    /// New Addy
+    console.log(address);
+  }
+
   render() {
-    console.log(this.state);
+    console.log(initialFlash(5));
+
     return <div>Herro</div>;
   }
 }
