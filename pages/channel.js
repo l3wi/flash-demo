0bf781e9ca75315b9ea11b0f5684735fd063c6d6
Lewis Freiberg <lewi@2pk.in>
2017-08-06 00:30:20 +1000

    Clean up channel page

--- pages/channel.js
+++ pages/channel.js
@@ -4,16 +4,6 @@ import styled from "styled-components";
 import { seedGen } from "../libs/flash/iota";
 
 export default class extends React.Component {
-  state = {
-    one: { seed: seedGen(81), input: 50 },
-    two: { seed: seedGen(81), input: 50 },
-    flash: {
-      depth: 3,
-      counter: [0, 0, 0, 0],
-      bundles: [],
-      genesis: "ehhel"
-    }
-  };
   render() {
     console.log(this.state);
     return <div>Herro</div>;
