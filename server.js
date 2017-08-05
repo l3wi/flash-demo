96c827fa7dece6daad2abd97de115dd3d4c60ad2
Lewis Freiberg <lewi@2pk.in>
2017-08-05 16:07:07 +1000

    Setup basic routes and pages

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const next = require("next");
+const routes = require("./routes");
+const app = next({ dev: process.env.NODE_ENV !== "production" });
+const handler = routes.getRequestHandler(app);
+
+// Without express
+const { createServer } = require("http");
+app.prepare().then(() => {
+  createServer(handler).listen(3000);
+});
