58f1e1bdff34dc1c3cda5825f9a82c518311781d
Lewis Freiberg <lewi@2pk.in>
2017-08-07 19:10:01 +1000

    Auto stash before merge of "master" and "origin/master"

--- libs/flash/webrtc.js
+++ libs/flash/webrtc.js
@@ -2,21 +2,21 @@ import { isClient } from '../utils'
 const EventEmitter = require('eventemitter3')
 import Flash from "./index";
 
-var Peer
-if(isClient) {
-  Peer = require('peerjs')
+var Peer;
+if (isClient) {
+  Peer = require("peerjs");
 }
 
 export default class WebRTC {
   constructor() {
-    this.connections = {}
-    this.events = new EventEmitter()
+    this.connections = {};
+    this.events = new EventEmitter();
   }
 
   async createAddress(roomData) {
-    var _this = this
+    var _this = this;
     return new Promise(function(resolve, reject) {
-      if(roomData.isMaster) {
+      if (roomData.isMaster) {
         // The master can just create an address and pass it on to slave to sign
         var newFlash = Flash.master.newAddress(roomData.mySeed, roomData.flashState)
         let eventFn = (message) => {
@@ -46,8 +46,8 @@ export default class WebRTC {
         }
         this.events.on('message', eventFn)
         this.broadcastMessage({
-          cmd: 'createAddress'
-        })
+          cmd: "createAddress"
+        });
       }
     });
   }
@@ -56,100 +56,101 @@ export default class WebRTC {
     // Keep trying to connect to a peer id
     // consisting of <channel address> + <peer number>
     // Peer number will keep increasing by until we hit a nonexisting address
-    var _this = this
+    var _this = this;
     return new Promise(function(resolve, reject) {
-      var peerNumber = 0
+      var peerNumber = 0;
       var getCurrentId = () => {
-        return `${_this.channel.roomId}-${peerNumber}`
-      }
+        return `${_this.channel.roomId}-${peerNumber}`;
+      };
       var tryCreateId = () => {
-        var tryId = getCurrentId()
-        var peer = new Peer(tryId, WebRTC.signalingServer)
-        var errorFn = (e) => {
-          if(e.type === 'unavailable-id') {
-            peer.destroy()
-            peerNumber++
-            tryCreateId()
+        var tryId = getCurrentId();
+        var peer = new Peer(tryId, WebRTC.signalingServer);
+        var errorFn = e => {
+          if (e.type === "unavailable-id") {
+            peer.destroy();
+            peerNumber++;
+            tryCreateId();
           }
-        }
+        };
         var openFn = () => {
-          peer.off('open', openFn)
-          peer.off('error', errorFn)
-          resolve(peer)
-        }
-        peer.on('error', errorFn)
-        peer.on('open', openFn)
-      }
-      tryCreateId()
+          peer.off("open", openFn);
+          peer.off("error", errorFn);
+          resolve(peer);
+        };
+        peer.on("error", errorFn);
+        peer.on("open", openFn);
+      };
+      tryCreateId();
     });
   }
 
   connectToPeers() {
-    for(var i = 0; i < 5; i++) {
-      var tryId = `${this.channel.roomId}-${i}`
-      if(tryId !== this.peer.id && (typeof this.connections[tryId] === 'undefined')) {
+    for (var i = 0; i < 5; i++) {
+      var tryId = `${this.channel.roomId}-${i}`;
+      if (
+        tryId !== this.peer.id &&
+        typeof this.connections[tryId] === "undefined"
+      ) {
         var conn = this.peer.connect(tryId, {
           reliable: true
-        })
-        this.onConnection(conn)
+        });
+        this.onConnection(conn);
       }
     }
   }
 
   async initChannel(channel) {
-    if(isClient) {
-      this.channel = channel
+    if (isClient) {
+      this.channel = channel;
 
-      this.peer = await this.getProbabilisticPeer()
-      this.peer.on('error', this.onError)
-      this.peer.on('close', this.onClose)
-      this.peer.on('disconnected', this.onDisconnected)
-      this.peer.on('connection', this.onConnection.bind(this))
-      console.log('connected to signaling server as peer id ' + this.peer.id);
+      this.peer = await this.getProbabilisticPeer();
+      this.peer.on("error", this.onError);
+      this.peer.on("close", this.onClose);
+      this.peer.on("disconnected", this.onDisconnected);
+      this.peer.on("connection", this.onConnection.bind(this));
+      console.log("connected to signaling server as peer id " + this.peer.id);
     }
   }
 
   broadcastMessage(message) {
-    for(var k in this.connections) {
-      var conn = this.connections[k]
-      conn.send(JSON.stringify(message))
+    for (var k in this.connections) {
+      var conn = this.connections[k];
+      conn.send(JSON.stringify(message));
     }
   }
 
   onConnection(conn) {
-    var _this = this
-    conn.on('open', () => {
-      _this.connections[conn.peer] = conn
+    var _this = this;
+    conn.on("open", () => {
+      _this.connections[conn.peer] = conn;
       console.log(`connected to ${conn.peer}`);
-      _this.events.emit('peerJoined', {
+      _this.events.emit("peerJoined", {
         connection: conn
-      })
-    })
-    conn.on('close', () => {
-      delete _this.connections[conn.peer]
-      _this.events.emit('peerLeft', {
+      });
+    });
+    conn.on("close", () => {
+      delete _this.connections[conn.peer];
+      _this.events.emit("peerLeft", {
         connection: conn
-      })
-    })
-    conn.on('data', (data) => {
-      _this.events.emit('message', {
+      });
+    });
+    conn.on("data", data => {
+      _this.events.emit("message", {
         connection: conn,
         data: JSON.parse(data)
-      })
-    })
+      });
+    });
   }
 
   onDisconnected() {
-    this.peer.reconnect()
+    this.peer.reconnect();
   }
 
   onOpen(connection) {
     console.log(connection);
   }
 
-  onClose() {
-
-  }
+  onClose() {}
 
   onError(error) {
     if(error.type !== 'peer-unavailable') {
@@ -159,8 +160,8 @@ export default class WebRTC {
 }
 
 WebRTC.signalingServer = {
-  host: 'localhost',
+  host: "localhost",
   port: 3000,
-  path: '/peerjs'
-}
-Object.freeze(WebRTC.signalingServer)
+  path: "/peerjs"
+};
+Object.freeze(WebRTC.signalingServer);
