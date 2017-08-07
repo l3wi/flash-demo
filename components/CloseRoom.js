ed64ae58c0ccab5e2b81f4f345a11708237d046b
Peter <peter@codebuffet.co>
2017-08-07 22:50:09 +0200

    closeroom event finished

--- components/CloseRoom.js
+++ components/CloseRoom.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import { seedGen, startAddresses, closeAddresses } from "../libs/flash/iota";
 import { isClient, get, set } from '../libs/utils'
+import { iota } from "../libs/iota-node";
 import Flash from "../libs/flash";
 
 export default class extends React.Component {
@@ -11,13 +12,18 @@ export default class extends React.Component {
   }
 
   closeRoom() {
-    
+    // todo: get bundles...
+    // todo: sign of all the balance from the remainder 50/50 to the 2 peers
+    iota.api.sendTransfer(this.props.roomData.mySeed, 4, 10, transfers, (e, bundle) => {
+      if (e) throw e;
+      console.log("Successfully sent your transfer: ", bundle);
+    })
   }
 
   render() {
     return (
       <div>
-        Closing the room means that both parties will get their final balance and the channel will be destroyed
+        Closing the room means that both parties will get their final balance and the channel will be destroyed. Are you sure you want to close?
         <br />
         <input value="Close Room" onClick={this.closeRoom.bind(this)} type="button"></input>
       </div>
