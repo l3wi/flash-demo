3b71e4e8b3d71cfc504b8aedca12fab6c0261ae0
Peter <peter@codebuffet.co>
2017-08-11 19:23:03 +0200

    fixed signature error

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,10 @@ import styled from "styled-components";
 import { seedGen, startAddresses, closeAddresses } from "../libs/flash/iota";
 import Flash from "../libs/flash";
 
-import { webRTC } from "../libs/flash";
-
 export default class extends React.Component {
   state = {
-    one: "",
-    two: "",
     flash: {},
+    seeds: {},
     total: { master: 50, slave: 50 },
     remainder: 100
   };
@@ -19,17 +16,21 @@ export default class extends React.Component {
     // Bundddles
   }
 
-  startChannel = async transactions => {
-    var initial = {
-      one: seedGen(81),
-      two: seedGen(81)
+  startChannel = async (maxTransactions) => {
+    if(isNaN(maxTransactions)) {
+      maxTransactions = 100
+    }
+    console.log(maxTransactions);
+    var seeds = {
+      master: seedGen(81),
+      slave:  seedGen(81)
     };
     /// Act as Player & Create inital addresses
     var flash = Flash.master.initalize(
-      initial.one,
-      transactions,
+      seeds.master,
+      maxTransactions,
       50,
-      initial.one
+      'KFENNTAJQPKKL9TQISCID9ERYXQNGBJWEUIKVH9QEUSC9PLMFLJOGHYPYGSUZXRXHBAMAKWIFZWYCP9FYLTMKNOVEB'
     );
     flash.stake = {
       master: 50,
@@ -39,49 +40,15 @@ export default class extends React.Component {
     console.log('first half of room', flash);
 
     // Act as Player 2 & Sign those addeses
-    flash = Flash.slave.initalize(initial.two, flash, initial.two);
-    console.log('cosiging room2', flash);
+    flash = Flash.slave.initalize(seeds.slave, flash, 'KFENNTAJQPKKL9TQISCID9ERYXQNGBJWEUIKVH9QEUSC9PLMFLJOGHYPYGSUZXRXHBAMAKWIFZWYCP9FYLTMKNOVEE');
 
     flash.index = 1
     flash.stake = {
       master: 50,
       slave: 50
     }
-    console.log('creating test tx');
-    var amount = 20
-    for(var key of Object.keys(flash.stake)) {
-      flash.stake[key] -= amount
-      flash.total[key] += amount
-    }
-    flash.total[from] -= amount
-    flash.total[to] += amount
-
-    flash = await Flash.master.newTransaction(
-      flash,
-      initial.one
-    );
-
-    // Sign those transactionbundles
-    flash = await Flash.slave.closeTransaction(flash, initial.two);
     console.log('updated flash', flash);
-    this.setState({ flash, ...initial });
-  };
-
-  newTransaction = async (one, two, flash) => {
-    // New trany from Player 1
-    flash = Flash.master.newAddress(one, flash);
-    // Confirm transaction as Player 2
-    flash = Flash.slave.closeAddress(one, flash);
-    // Start new transaction
-    flash = await Flash.master.newTransaction(
-      flash,
-      { master: 0, slave: 0 },
-      one
-    );
-    // Finsh signing the bundles
-    flash = await Flash.slave.closeTransaction(flash, two);
-    console.log("Updated Bundle: ", flash);
-    this.setState({ flash });
+    this.setState({ flash, seeds });
   };
 
   closeChannel = async (one, two, flash) => {
@@ -91,32 +58,35 @@ export default class extends React.Component {
     console.log("Updated Bundle: ", flash);
   };
 
-  send = (uid, amount, flash) => {
-    flash.balance.remainder = flash.balance.remainder - amount * 2;
-    if ("master") {
-      flash.balance.master = flash.balance.master + amount;
-      flash.balance.slave = flash.balance.slave - amount;
-    } else {
-      flash.balance.master = flash.balance.master - amount;
-      flash.balance.slave = flash.balance.slave + amount;
+  send = (to, from, amount, flash, seeds) => {
+    for(var key of Object.keys(flash.stake)) {
+      flash.stake[key] -= amount
+      flash.total[key] += amount
     }
-    if (flash.balance.remainder <= 0)
-      return alert(
-        "You have no more balance. You should close this channel nowish.s"
-      );
-    this.setState({ ...flash });
+    flash.total[from] -= amount
+    flash.total[to] += amount
+    var remainder = Object.values(flash.stake).reduce((sum, value) => sum + value)
+    if(remainder < 0) {
+      alert("This flash channel has no transportable balance left. The room should be closed.")
+      return
+    }
+
+    (async() => {
+      flash = await Flash.master.newTransaction(flash, seeds[to])
+    })()
   };
 
   render() {
-    var { one, two, flash, transactions } = this.state;
+    var { seeds, flash, transactions } = this.state;
     return (
       <Wrapper>
         <div>
           <h4>Flash Object</h4>
           <div>
+            Max number of transactions (defaults to 100)<br />
             <input
               value={transactions}
-              placeholder={`Max number of transactions`}
+              type='number'
               onChange={data =>
                 this.setState({ transactions: data.target.value })}
             />
@@ -125,10 +95,6 @@ export default class extends React.Component {
             </button>
           </div>
           <div>
-            <button onClick={() => this.newTransaction(one, two, flash)}>
-              New Transaction
-            </button>
-
             <p>
               {/* Balance left: {flash && flash.balance.remainder} */}
             </p>
@@ -146,18 +112,18 @@ export default class extends React.Component {
         <div>
           <h2>Player 1</h2>
           <p>
-            Seed: {one && one.substring(0, 10)}...
+            Seed: {seeds.master && seeds.master.substring(0, 10)}...
           </p>
-          <button onClick={() => this.send("master", 10, flash)}>
+          <button onClick={() => this.send("master", "slave", 10, flash, seeds)}>
             Send 10
           </button>
         </div>
         <div>
           <h2>Player 2</h2>
           <p>
-            Seed: {two && two.substring(0, 10)}...
+            Seed: {seeds.slave && seeds.slave.substring(0, 10)}...
           </p>
-          <button onClick={() => this.send("slave", 5, flash)}>Send 5</button>
+          <button onClick={() => this.send("master", "slave", 10, flash, seeds)}>Send 10</button>
         </div>
       </Wrapper>
     );
