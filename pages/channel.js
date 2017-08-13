eaf405cea0a140a7a4c8e2f35c6bf412e0d443bf
Lewis Freiberg <lewi@2pk.in>
2017-08-13 17:49:43 +1000

    Shuffle the pages around

--- pages/channel.js
+++ pages/channel.js
@@ -1,381 +1,134 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
+import { Layout, LeftContent, RightContent } from "../components/layout"
+import Channel from "../components/channel"
+import Setup from "../components/channelSetup"
 
-import { seedGen, startAddresses, closeAddresses } from "../libs/flash/iota";
-import { webRTC } from "../libs/flash"
-import { isClient, get, set } from '../libs/utils'
-import { iota } from "../libs/iota-node";
-import Flash from "../libs/flash";
-import InitRoom from '../components/InitRoom'
-import CloseRoomComponent from '../components/CloseRoom'
-import MakeTransaction from '../components/MakeTransaction'
-import Deposit from '../components/Deposit'
-import CloseRoom from '../libs/flash/close-room.js'
+const SideBar = () =>
+  <RightContent>
+    <h3>Channel History</h3>
+    <History>
+      {[
+        "Waiting for a mate...",
+        "Mate connected.",
+        "Signing addresses",
+        "Deposit address generated",
+        "1st deposit complete",
+        "2nd deposit complete",
+        "Channel is now setup",
+        "Partner has initiated a transfer of 50 to themself",
+        "You confirmed the transfer of 50"
+      ]
+        .reverse()
+        .map(item =>
+          <Item>
+            {item}
+          </Item>
+        )}
+    </History>
+  </RightContent>
 
 export default class extends React.Component {
-  closeRoom = new CloseRoom()
+  static async getInitialProps({ query }) {
+    return query
+  }
+
   state = {
-    status: 'loaded',
-    peers: [],
-    roomData: {
-      index: -1,
-      mySeed: null,
-      flashState: null,
-      fullDepositMade: false
-    }
-  }
-
-  connectToPeersTimer = null
-
-  tryGetRoomData() {
-    this.setState({
-      roomData: Object.assign(this.state.roomData, get(`room-${this.props.url.query.id}`))
-    })
-
-    // Remove later
-    var _this = this
-    window.setState = (s) => {
-      _this.didMakeSuccessfulTransaction(s)
-    }
-  }
-
-  storeRoomDataLocally(roomData = this.state.roomData) {
-    set(`room-${this.props.url.query.id}`, roomData)
-  }
-
-  clearConnectTimer() {
-    if(typeof this.connectToPeersTimer !== null) {
-      clearInterval(this.connectToPeersTimer)
-      this.connectToPeersTimer = null
-    }
-  }
-
-  componentWillUnmount() {
-    this.clearConnectTimer()
-  }
-
-  handleMessage(message) {
-    if(message.cmd === 'signAddress' && this.state.roomData.index != 0) {
-      // co-sign the address as the slave
-      var newFlashState = Flash.slave.closeAddress(this.state.roomData.mySeed, message.flashState)
-      webRTC.broadcastMessage({
-        cmd: 'signAddressResult',
-        flashState: newFlashState
-      })
-    }
-
-    if(message.cmd === 'signTransaction' && this.state.roomData.index != 0) {
-      // Finsh signing the bundles
-      (async() => {
-        var newFlashState = await Flash.slave.closeTransaction(message.flashState, this.state.roomData.mySeed)
-        // Dirty, temporary workaround. So we can rely on a good callback for slave.
-        this.didMakeSuccessfulTransaction(newFlashState)
-        webRTC.events.emit('signTransactionResult', newFlashState)
-        webRTC.broadcastMessage({
-          cmd: 'signTransactionResult',
-          flashState: newFlashState
-        })
-      })()
-    }
-
-    if(message.cmd === 'createTransaction' && this.state.roomData.index == 0) {
-      (async() => {
-        // True at the end make sure that if the slave asks the master to create a transaction
-        // the amount is always sent to master (since in essence, slave will be paying)
-        var createAddress = true
-        if('createAddress' in message) {
-          createAddress = message.createAddress
-        }
-        var from = "slave"
-        var to = "master"
-        var newFlashState = await webRTC.createTransaction(this.state.roomData, message.amount, to, from)
-        this.didMakeSuccessfulTransaction(newFlashState)
-      })()
-    }
-
-    if(message.cmd === 'closeChannel' && this.state.roomData.index == 0) {
-      (async() => {
-        await webRTC.closeChannel(this.state.roomData)
-      })()
-    }
-
-    if(message.cmd === 'signCloseChannel' && this.state.roomData.index == 1) {
-      (async() => {
-        var newFlashState = await Flash.slave.closeFinalBundle(message.flashState, this.state.roomData.mySeed)
-        this.didMakeSuccessfulTransaction(newFlashState)
-        webRTC.broadcastMessage({
-          cmd: 'signCloseChannelResult',
-          flashState: newFlashState
-        })
-        var results = await this.closeRoom.attachAndPOWClosedBundle(newFlashState.finalBundles)
-        for(var bundle of results) {
-          console.log(
-            "attachAndPOWClosedBundle > validateSignatures",
-            iota.utils.validateSignatures(bundle, newFlashState.addresses[0].address)
-          );
-        }
-      })()
-    }
-
-    if(message.cmd === 'signCloseChannelResult' && this.state.roomData.index == 0) {
-      this.didMakeSuccessfulTransaction(message.flashState)
-    }
-
-    if(message.cmd === 'didDeposit') {
-      this.state.roomData.flashState = message.flashState
-      this.storeRoomDataLocally()
-      this.setState({
-        roomData: this.state.roomData
-      })
-    }
-
-    if(message.cmd === 'initRoomResult' && this.state.roomData.index == 0) {
-      this.state.roomData.flashState = message.flashState
-      this.setState({
-        roomData: this.state.roomData
-      })
-      this.storeRoomDataLocally()
-    }
-
-    if(message.cmd === 'initRoom' && this.state.roomData.index == -1) {
-      var mySeed = seedGen(81)
-      // Now we need to co-sign the room
-      var settlementAddress = prompt('Please enter your settlement address')
-      console.log('settlementAddress (prompt)', settlementAddress);
-      var newFlashState = Flash.slave.initalize(mySeed, message.flashState, settlementAddress)
-      console.log('initalized', JSON.stringify(newFlashState));
-      var roomData = {
-        flashState: newFlashState,
-        mySeed,
-        index: 1 // hardcoded for now (this.state.peers.length seems to fail sometimes)
-      }
-      this.setState({
-        roomData
-      })
-      this.storeRoomDataLocally(roomData)
-
-      // Now send the new state back to the other peer
-      webRTC.broadcastMessage({
-        cmd: 'initRoomResult',
-        flashState: newFlashState
-      })
-    }
-  }
-
-  initWebRTC() {
-    (async() => {
-      var _this = this
-      // Super huge hack. Don't try at home
-      webRTC.channel = this
-      var result = await webRTC.initChannel({
-        roomId: _this.props.url.query.id
-      })
-      console.log('initChannel result', result);
-      // Keep trying to find new peers
-      _this.connectToPeersTimer = setInterval(() => {
-        webRTC.connectToPeers()
-      }, 1000)
-      webRTC.events.on('message', (message) => {
-        console.log(`${message.connection.peer}:`, message.data)
-        _this.handleMessage(message.data)
-        if(_this.state.roomData.index == 0) {
-          Flash.master.handleMessage(message.data)
-        }
-        else {
-          Flash.slave.handleMessage(message.data)
-        }
-      })
-
-      webRTC.events.on('peerLeft', () => {
-        _this.setState({
-          peers: Object.values(webRTC.connections)
-        })
-      })
-
-      webRTC.events.once('peerJoined', ({ connection }) => {
-        if(Object.values(webRTC.connections).length > 0) {
-          _this.setState({
-            status: 'peer-joined',
-            peers: Object.values(webRTC.connections)
-          })
-          _this.clearConnectTimer()
-        }
-      })
-    })()
-  }
-
-  didMakeSuccessfulTransaction(flashState) {
-    console.log('didMakeSuccessfulTransaction', flashState);
-    this.state.roomData.flashState = flashState
-    this.setState({
-      roomData: this.state.roomData
-    })
-    this.storeRoomDataLocally()
+    setup: false,
+    form: 0,
+    transactions: 0,
+    address: "",
+    peer: false
   }
 
   componentDidMount() {
-    if(isClient) {
-      this.tryGetRoomData()
-      this.initWebRTC()
-    }
+    setTimeout(() => {
+      this.setState({ form: 1 })
+    }, 300)
   }
 
-  msgKeyPress(e) {
-    if (e.key === 'Enter') {
-      webRTC.broadcastMessage({
-        data: e.currentTarget.value
-      })
-      e.currentTarget.value = ''
-    }
-  }
-
-  renderBalance() {
-    if(this.state.roomData.flashState !== null) {
-      return (<div>
-        Balance: (master: { this.state.roomData.flashState.total['master'] } slave: { this.state.roomData.flashState.total['slave'] })<br />
-        Stake: (master: { this.state.roomData.flashState.stake['master'] } slave: { this.state.roomData.flashState.stake['slave'] })<br />
-        Remainder: { Object.values(this.state.roomData.flashState.stake).reduce((sum, value) => sum + value, 0) }
-      </div>)
-    }
-  }
-
-  renderStatus() {
-    return (
-      <div>
-        Status: { this.state.status }<br />
-        { this.renderBalance() }
-      </div>
-    )
-  }
-
-  didDeposit() {
-    this.state.roomData.fullDepositMade = true
-    this.state.roomData.flashState.stake[this.state.roomData.index == 0? "master" : "slave"] += this.state.roomData.flashState.depositAmount
-    webRTC.broadcastMessage({
-      cmd: 'didDeposit',
-      flashState: this.state.roomData.flashState
-    })
-    this.storeRoomDataLocally()
-    this.setState({
-      roomData: this.state.roomData
-    })
-  }
-
-  shouldCloseRoom() {
-    return Object.values(this.state.roomData.flashState.stake).reduce((sum, value) => sum + value) == 0
-  }
-
-  allPeersDeposited() {
-    if(typeof this.state.roomData.flashState === undefined || this.state.roomData.flashState === null) {
-      return false
-    }
-    var totalInRoom =
-        Object.values(this.state.roomData.flashState.stake).reduce((sum, value) => sum + value)
-      + Object.values(this.state.roomData.flashState.total).reduce((sum, value) => sum + value)
-    return totalInRoom === (this.state.roomData.flashState.depositAmount * 2)
-  }
-
-  initialRoomMade() {
-    // Checking if mySeed is null
-    // means that you haven't generated any private room data from this room yet.
-    // we can assume that there was no initial data made yet.
-    return this.state.roomData.mySeed !== null
-  }
-
-  initializeRoomCallback(roomData) {
-    // We also move back to loaded-state
-    // This makes us wait for another peer again, which is fine now. We are the creator.
-    this.setState({
-      roomData,
-      status: 'loaded'
-    })
-  }
-
-  renderInit() {
-    if(this.state.status === 'init') {
-      return (<InitRoom callback={ this.initializeRoomCallback.bind(this) }></InitRoom>)
-    }
-  }
-
-  renderWait() {
-    if(isClient) {
-      if(!this.initialRoomMade() && (this.state.status === 'loaded' || this.state.status === 'peer-joined')) {
-        return (<div>
-          We haven't found any local room data yet. You can wait until a peer joins who does, or initialize the room yourself.
-          <br />
-          <input type="button" onClick={() => { this.setState({ status: 'init' }) }} value="Initialize"></input>
-        </div>)
-      }
-    }
-  }
-
-  renderFlashObjectDebug() {
-    if(this.initialRoomMade()) {
-      var flash = this.state.roomData.flashState
-      return (
-        <div>
-          <h4>Flash Object</h4>
-          <p>
-            Depth: {flash.depth} Address Index: {flash.addressIndex}
-          </p>
-          {flash.addresses &&
-            flash.addresses.map((level, index) =>
-              <div key={index}>
-                <strong>
-                  Level: {index}
-                </strong>
-                <p>
-                  {level.address && level.address.substring(0, 10)} ...
-                </p>
-              </div>
-            )}
-        </div>
-      )
-    }
-  }
-
-  renderClose() {
-    if(this.state.status === 'close-room') {
-      return (<CloseRoomComponent roomData={this.state.roomData}></CloseRoomComponent>)
-    }
-  }
-
-  renderDeposit() {
-    if(!this.state.roomData.fullDepositMade && this.initialRoomMade()) {
-      return (<Deposit callback={this.didDeposit.bind(this)} roomData={this.state.roomData}></Deposit>)
-    }
-  }
-
-  renderCreateTransaction() {
-    if(this.state.status === 'make-transaction') {
-      return (<MakeTransaction callback={this.didMakeSuccessfulTransaction.bind(this)} roomData={this.state.roomData}></MakeTransaction>)
-    }
-  }
-
-  renderButtons() {
-    if(this.allPeersDeposited()) {
-      return (<div>
-        <input disabled={this.shouldCloseRoom()} type="button" onClick={() => { this.setState({ status: 'make-transaction' }) }} value="Make Transaction"></input>
-        <input type="button" onClick={() => { this.setState({ status: 'close-room' }) }} value="Close Room"></input>
-      </div>)
-    }
+  setChannel = (address, transactions, deposits) => {
+    this.setState({ setup: true })
   }
 
   render() {
+    var { form, peer, setup } = this.state
+    console.log(this.props)
     return (
-      <div>
-        { this.renderButtons() }
-        <br />
-        Hello! We are the <b>{ ['nothing', 'master', 'slave'][this.state.roomData.index + 1] }</b> connected to { this.state.peers.length } peers!
-        <br />
-        { this.renderStatus() }
-        { this.renderWait() }
-        { this.renderInit() }
-        { this.renderDeposit() }
-        { this.renderClose() }
-        { this.renderCreateTransaction() }
-        { this.renderFlashObjectDebug() }
-      </div>
+      <Layout right={setup && SideBar()}>
+        <LeftContent noBg={!setup}>
+          {!setup ? <Setup setChannel={this.setChannel} /> : <Channel />}
+        </LeftContent>
+      </Layout>
     )
   }
 }
+
+const Row = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: ${props => (props.border ? "2px solid #222" : "none")};
+`
+const Field = styled.input`
+  flex: 1;
+  background: none;
+  font-size: 100%;
+  padding: 5px 0px;
+  width: 100%;
+  border: none;
+  color: white;
+  border-bottom: 2px solid #222;
+  &:focus {
+    outline: none;
+  }
+`
+const Button = styled.button`
+  padding: 15px 20px;
+  background: #d30c7b;
+  border: none;
+  color: white;
+  font-weight: 600;
+  margin: 0 2rem;
+  &:focus {
+    outline: none;
+  }
+`
+
+const AnimatedLeftBox = styled.span`
+  position: absolute;
+  width: 100%;
+  transition: all .5s ease;
+  transform: ${props =>
+    props.active ? "translateY(0px)" : "translateY(20px)"};
+  visibility: ${props => (props.active ? "visible" : "hidden")};
+  opacity: ${props => (props.active ? "1" : "0")};
+`
+
+const History = styled.div`
+  border-top: 2px solid white;
+  margin: 0 0 5px;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  flex: 1;
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 14px;
+    width: 90%;
+    height: 2rem;
+    background: linear-gradient(
+      to bottom,
+      rgba(180, 180, 180, 0),
+      rgba(180, 180, 180, 1)
+    );
+  }
+`
+const Item = styled.p`
+  padding-bottom: 5px;
+  border-bottom: 1px solid rgba(255, 255, 255, .5);
+`
--- pages/index.js
+++ pages/index.js
@@ -4,33 +4,9 @@ import { Layout, LeftContent, RightContent } from "../components/layout"
 import { Link, Router } from "../routes"
 import shortid from "shortid"
 
-const Button = styled.button`
-  padding: 15px 20px;
-  background: #d30c7b;
-  border: none;
-  color: white;
-  font-weight: 600;
-  &:focus {
-    outline: none;
-  }
-`
-
-const AnimatedLeftBox = styled.span`
-  position: absolute;
-  padding: 10px;
-  transition: all .5s ease;
-  transform: ${props =>
-    props.active ? "translateY(0px)" : "translateY(20px)"};
-  visibility: ${props => (props.active ? "visible" : "hidden")};
-  opacity: ${props => (props.active ? "1" : "0")};
-`
-
 export default class extends React.Component {
   state = {
-    form: 0,
-    transactions: "",
-    address: "",
-    deposit: ""
+    form: 0
   }
 
   componentDidMount() {
@@ -40,27 +16,15 @@ export default class extends React.Component {
   }
 
   startChannel = (address, transactions, deposit) => {
+    this.setState({ form: 0 })
     const channelID = shortid.generate()
-    console.log(channelID)
-
-    if (transactions <= 3)
-      return alert(
-        "Need more than 3 transaction to make a Flash channel worth while."
-      )
-
-    if (deposit < 0)
-      return alert(
-        "The whole point is to send IOTA. Deposits must be larger than 0"
-      )
-
-    if (address.length !== 81 && address.length !== 90)
-      return alert("Address not long enough")
-
-    Router.pushRoute(`/channel/${channelID}`, { transactions })
+    setTimeout(() => {
+      Router.pushRoute(`/channel/${channelID}`)
+    }, 500)
   }
 
   render() {
-    var { form, transactions, address, deposit } = this.state
+    var { form } = this.state
     return (
       <Layout>
         <LeftContent noBg>
@@ -73,51 +37,7 @@ export default class extends React.Component {
               {`In Flash users can create and send transactions between each other off the
       network without doing Proof of Work while remaining secure. `}
             </p>
-            <Button onClick={() => this.setState({ form: 2 })}>
-              Start a channel
-            </Button>
-          </AnimatedLeftBox>
-
-          <AnimatedLeftBox active={form === 2}>
-            <h2>Setup Channel</h2>
-
-            <div>
-              <Field
-                type="text"
-                value={transactions}
-                onChange={data =>
-                  this.setState({ transactions: data.target.value })}
-                placeholder={`Enter maxium transactions`}
-              />
-            </div>
-
-            <p>
-              {`Flash requires a maximum number of transactions to be defined 
-            before hand. Try 100 to start.`}
-            </p>
-            <div>
-              <Field
-                type="text"
-                placeholder={`Enter your inital deposit amount`}
-              />
-            </div>
-            <p
-            >{`Specify the amount that you will deposit into the channel to use for transfering. Your partner will have to deposit the same amount.`}</p>
-            <div>
-              <Field
-                type="text"
-                value={address}
-                onChange={data => this.setState({ address: data.target.value })}
-                placeholder={`Enter your settlement address`}
-              />
-            </div>
-            <p
-            >{`This is the address your final balance will be deposited into when the channel is complete`}</p>
-            <Button
-              onClick={() => this.startChannel(address, transactions, deposit)}
-            >
-              Launch Channel
-            </Button>
+            <Button onClick={() => this.startChannel()}>Start a channel</Button>
           </AnimatedLeftBox>
         </LeftContent>
       </Layout>
@@ -141,4 +61,24 @@ const Field = styled.input`
   &:focus {
     outline: none;
   }
+`
+const Button = styled.button`
+  padding: 15px 20px;
+  background: #d30c7b;
+  border: none;
+  color: white;
+  font-weight: 600;
+  &:focus {
+    outline: none;
+  }
+`
+
+const AnimatedLeftBox = styled.span`
+  position: absolute;
+  padding: 10px;
+  transition: all .5s ease;
+  transform: ${props =>
+    props.active ? "translateY(0px)" : "translateY(20px)"};
+  visibility: ${props => (props.active ? "visible" : "hidden")};
+  opacity: ${props => (props.active ? "1" : "0")};
 `
