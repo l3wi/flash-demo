e19c6fd5ccea51d88ec581fea47a764ff9c021e8
Lewis Freiberg <lewi@2pk.in>
2017-09-19 12:01:16 +1000

    Use GTM instead

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Head, Main, NextScript } from "next/document"
 import { ServerStyleSheet, injectGlobal } from "styled-components"
 
+const GA_TRACKING_ID = "UA-106570081-1"
+
 export default class MyDocument extends Document {
   render() {
     const sheet = new ServerStyleSheet()
@@ -13,8 +15,17 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <script src={"/static/curl.min.js"} />
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
             dangerouslySetInnerHTML={{
-              __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-106570081-1', 'auto');ga('send', 'pageview');`
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)};
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+              `
             }}
           />
           <link rel="icon" type="image/png" href="/static/favicon.ico" />
